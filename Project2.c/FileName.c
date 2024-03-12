dfdb3bd3c4708f9859272ae65521cbdd0eea6e80
한범 김 <mine5139@naver.com>
2024-03-12 10:43:39 +0900

    10 43

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-	int i = 0;
+	int i;
 	int sum;
+	int count= 0
 	sum = 0;
-	if (i % 3 = 0);
-	return i;
-	else if (i % 2 = 0);
-	return i;
-	else
-		i = 0;
+	for (i = 2; i <= 100; i++)
+		if (i % 3 == 0); return i;
 
+		else if (i % 2 == 0); return i;
 
-	for (i = 1; i <= 100; i++)
+		else return 0;
+			
 		sum += i;
 
 	printf("1에서 100까지 소수의 합=%d", sum);
