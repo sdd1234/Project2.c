a975dcc0d13bdaaa290517754530841059b7d9f5
한범 김 <mine5139@naver.com>
2024-03-13 22:25:27 +0900

    3 13 10 25

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-	int i = 0;
-	int j = 2;
-	int k = 3;
-	printf("1에서 100까지 소수의 합은? 1제외");
-	
-	for (i = 2; i <= 100; i++)
-	    if (i % j == 0)
-			return 0;
-	    else if (i % k == 0);
-	        return 0;
-		else printf("%4d", i);
-	     
+    int i = 0;
+    int j = 0;
+    int cou = 0;
+    int sum = 0;
+
+    for (i = 2; i < 100; i++)
+        for (j = 2; j < i; j++)
+            if (i % j == 0)
+                cou++;
+    if (cou == 2)
+        sum += i;
+      printf("100까지의 소수의 합은 %d 입니다 ", j);
+
     return 0;
 }
