7f959f4a972fd0a0f39fcc0ad6551b44c1d87bff
한범 김 <mine5139@naver.com>
2024-03-13 23:19:50 +0900

    수정본 3 13 11 19

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -3,16 +3,22 @@
 int main(void) {
     int i = 0;
     int j = 0;
-    int cou = 0;
     int sum = 0;
 
     for (i = 2; i < 100; i++)
+    {
         for (j = 2; j < i; j++)
+        {
             if (i % j == 0)
-                cou++;
-    if (cou == 2)
-        sum += i;
-      printf("100까지의 소수의 합은 %d 입니다 ", j);
+                break;
+        }
+
+
+        if (i == j)
+            sum = +i;
+        printf("%d ", i);
+    }
+
 
     return 0;
 }
