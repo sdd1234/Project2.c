33cf070c69d6ab387fb12cf19f9620f33a8ca5f0
한범 김 <mine5139@naver.com>
2024-03-14 15:31:49 +0900

    답이 0이나와서 지금 문제

--- Project2.c/5701653.c
+++ Project2.c/5701653.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void) {
     int i = 0;
     int j = 0;
     int sum = 0;
+    clock_t start, stop; //clock 함수 시작과 끝을 start stop로 지정
+    double d;           //실수값이 나오기때문에 double로 생성
+    start = clock();        //측정시작
 
-    for (i = 2; i < 100; i++)
+    for (i = 2; i < 100; i++)    //중첩 for문  2부터 시작해서 99까지 2부터 99까지의 숫자로 나누어보고 나머지가 0이면  break 로 for문을 빠져나옴
     {
         for (j = 2; j < i; j++)
         {
@@ -14,12 +18,14 @@ int main(void) {
         }
 
 
-        if (i == j) {
-            sum +=i;
-            printf("%d ", i);
+        if (i == j) {          //빠져나왓을때 나머지가 0인 숫자가 자기자신이면 모두 sum에 값을 더해서 저장한다
+            sum += i;
+
         }
     }
-
-    printf("%d", sum);
+    stop = clock();                //측정을 종료 
+    d = (double)(stop - start) / CLOCKS_PER_SEC;
+    printf("100까지의 소수의합은%d입니다", sum);
+    printf("측정시간은 %f초이다", d);                                        //소수의 합과 측정시간을 도출
     return 0;
 }
