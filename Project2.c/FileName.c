15b76468509fc84a8419157d4263a8a5bfeb8741
한범 김 <mine5139@naver.com>
2024-03-19 19:43:43 +0900

    1

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
+long long power_iter(long n,j)
+{ 
+    int i;
+    long long k;
+
+    for (i = 0; i < j; i++) {
+        k = k * n;
+        return k;
+
+
+
+    }
+
+
+
+
+}
+long long power_rec(long k)
+{
+
+
+
+
+}
+
 
 int main(void) {
-    int i = 0;
-    int j = 0;
-    int sum = 0;
-
-    for (i = 2; i < 100; i++)
-    {
-        for (j = 2; j < i; j++)
-        {
-            if (i % j == 0)
-                break;
-        }
-
-
-        if (i == j)
-            sum = +i;
-        printf("%d ", i);
-    }
+    
+    
 
 
     return 0;
