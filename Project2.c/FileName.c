7726db16d11e9158be8dcfcabd1419ca72cacbda
한범 김 <mine5139@naver.com>
2024-03-19 19:26:43 +0900

    7 26

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
-
-int main(void) {
-    int i = 0;
-    int j = 0;
-    int sum = 0;
-
-    for (i = 2; i < 100; i++)
+#include <time.h>
+int long long factorial_iter(long m)
+{
+    long i;
+    long long result = 1;
+    for (i = 1; i <= m; i++)
     {
-        for (j = 2; j < i; j++)
-        {
-            if (i % j == 0)
-                break;
-        }
-
-
-        if (i == j)
-            sum = +i;
-        printf("%d ", i);
+        result *= i;
     }
+    return result;
+}
+int  long long factorial_rec(long n)
+{
+    if (n <= 1)
+        return 1;
+    else {
+
+
+        return n * factorial_rec(n - 1);
+
+    }
+}
+int main(void) {
+    long long u = factorial_iter(20);
+    long long s = factorial_rec(20);
+    printf("1½Ä %lld 2½Ä %lld", u, s);
+
+
 
 
     return 0;
