c816cbb904a4d2d93b1cacbbe38554137f5faa0a
한범 김 <mine5139@naver.com>
2024-03-19 19:51:49 +0900

    미완성

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -4,19 +4,14 @@ long long power_iter(long n,j)
     int i;
     long long k;
 
-    for (i = 0; i < j; i++) {
+    for (i = 0; i < j; i++) 
         k = k * n;
         return k;
 
 
-
-    }
-
-
-
-
 }
-long long power_rec(long k)
+long long power_rec(long k,e)
+
 {
 
 
@@ -27,7 +22,8 @@ long long power_rec(long k)
 
 int main(void) {
     
-    
+    long long result_iter = power_iter(13, 21);
+    long long result_rec = power_rec(13, 21);
 
 
     return 0;
