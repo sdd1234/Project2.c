56970a9b656bb5758cfdb36ad24c5c4cf343f72c
한범 김 <mine5139@naver.com>
2024-03-19 10:58:44 +0900

    tnwjd

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
-
-int main(void) {
-    int i = 0;
-    int j = 0;
-    int sum = 0;
-
-    for (i = 2; i < 100; i++)
+#include <time.h>
+int long factorial_iter(long m)
+{
+    long i; 
+    long result = 1;
+    for (i = 1; i <= m; i++)
     {
-        for (j = 2; j < i; j++)
-        {
-            if (i % j == 0)
-                break;
+        result *= i;
         }
-
-
-        if (i == j)
-            sum = +i;
-        printf("%d ", i);
+    return result;
     }
+int  long factorial_rec(long n)
+{
+   if (n <= 1) 
+        return 1;
+     else{ 
+        
+       
+          return n * factorial_rec(n - 1);
+       
+    }
+ }
+int main(int argc, char* argv[]) {
+    clock_t start_iter, end_iter, start_rec, end_rec;
+    long result_iter, result_rec;
+     
+    start_iter = clock();
+    result_iter = factorial_iter(20);
+    end_iter = clock();
 
-
-    return 0;
+    start_rec = clock();
+    result_rec = factorial_rec(20);
+    start_rec = clock();
+    
+    
+   printf("걸린시간 %f", (float)(end_iter - start_iter) / CLOCKS_PER_SEC);
+   printf("걸린시간 %f", (float)(end_rec - start_rec) / CLOCKS_PER_SEC);
+   printf("%ld",result_iter);
+   printf("%ld", result_rec);
+       return 0;
 }
