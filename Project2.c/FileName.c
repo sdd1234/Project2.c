35a85f092c1130650558e7660e26e42961c9d0ea
한범 김 <mine5139@naver.com>
2024-03-23 15:54:12 +0900

    replit 에서 완성 5701653

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
-long long power_iter(long n,j)
-{ 
-    int i;
-    long long k;
-
-    for (i = 0; i < j; i++) 
-        k = k * n;
-        return k;
-
-
-}
-long long power_rec(long k,e)
-
+#include <time.h>
+double power_iter(int n, int j)
 {
-
-
-
-
+    double k = 1;
+    for (int i = 1; i <= j; i++) {
+        k *= n;
+    }
+    return k;
 }
-
+double power_rec(long n, j)
+{
+    double a = 1;
+    if (j == 0)
+        return 1;
+    else
+        return n * power_rec(n, j - 1);
+    a *= power_rec(n, j - 1);
+    return a;
+}
 
 int main(void) {
-    
-    long long result_iter = power_iter(13, 21);
-    long long result_rec = power_rec(13, 21);
-
-
+    clock_t start, stop;
+    double cai;
+    start = clock();
+    double result_iter = power_iter(13, 21);
+    printf("반복: %f\n", result_iter);
+    stop = clock();
+    cai = (double)(stop - start) / CLOCKS_PER_SEC;
+    printf("측정시간은 %f초.\n", cai);
+    start = clock();
+    double result_rec = power_rec(13, 21);
+    printf("재귀: %f\n", result_rec);
+    stop = clock();
+    cai = (double)(stop - start) / CLOCKS_PER_SEC;
+    printf("측정시간은 %f초.\n", cai);
     return 0;
 }
