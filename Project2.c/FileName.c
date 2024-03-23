0af290b8aec9e2147079421f0d5e221060ec8e22
한범 김 <mine5139@naver.com>
2024-03-23 14:43:20 +0900

    함수이름까지수정완료 5701653

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
 #include <time.h>
-int long long factorial_iter(long m)
+
+long long factorial_iter(int n)
 {
-    long i;
+    long long i;
     long long result = 1;
-    for (i = 1; i <= m; i++)
+    for (i = 1; i <= n; i++)
     {
         result *= i;
     }
     return result;
 }
-int  long long factorial_rec(long n)
+long long factorial_rec(int n)
 {
-    if (n <= 1)
+    long long k = 1;
+    if (n == 1)
         return 1;
-    else {
-
-
+    else
         return n * factorial_rec(n - 1);
 
-    }
 }
+
 int main(void) {
-    long long u = factorial_iter(20);
-    long long s = factorial_rec(20);
-    printf("1식 %lld 2식 %lld", u, s);
+    clock_t start, stop;
+    double cai;
+    start = clock();
+    long long result_iter = factorial_iter(20);
+    printf("%lld\n", result_iter);
+    stop = clock();
+    cai = (double)(stop - start) / CLOCKS_PER_SEC;
+    printf("반복 %f", cai);
 
-
-
-
+    start = clock();
+    long long result_rec = factorial_rec(20);
+    printf("%lld\n", result_rec);
+    stop = clock();
+    cai = (double)(stop - start) / CLOCKS_PER_SEC;
+    printf("순환 %f", cai);
     return 0;
 }
