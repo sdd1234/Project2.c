a28d72e308bcb18167b2c2628a1875e35355186b
한범 김 <mine5139@naver.com>
2024-04-02 23:12:57 +0900

    수정완료

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,24 +1,61 @@
 #include <stdio.h>
+#define MAX_DEGREE 200
 
-int main(void) {
-    int i = 0;
-    int j = 0;
-    int sum = 0;
+struct polynomial {
+    int degree;
+    int coef[MAX_DEGREE + 1];
+};
 
-    for (i = 2; i < 100; i++)
-    {
-        for (j = 2; j < i; j++)
-        {
-            if (i % j == 0)
-                break;
-        }
+struct polynomial multiply(struct polynomial a, struct polynomial b) {
+    struct polynomial result;
+    result.degree = a.degree + b.degree;
 
-
-        if (i == j)
-            sum = +i;
-        printf("%d ", i);
+    for (int i = 0; i <= result.degree; i++) {
+        result.coef[i] = 0;
     }
 
+    for (int i = 0; i <= a.degree; i++) {
+        for (int j = 0; j <= b.degree; j++) {
+            result.coef[i + j] += a.coef[i] * b.coef[j];
+        }
+    }
+
+    return result;
+}
+
+void print(struct polynomial p) {
+    int isFirstTerm = 1;
+
+    for (int i = p.degree; i >= 0; i--) {
+        if (p.coef[i] != 0) {
+            if (!isFirstTerm) {
+                printf(" + ");
+            }
+            else {
+                isFirstTerm = 0;
+            }
+            printf("%dx^%d", p.coef[i], i);
+        }
+    }
+    printf("\n");
+}
+
+int main(void) {
+    struct polynomial a = { 2, {1, 2, 1} };
+    struct polynomial b = { 1, {3, 1} };
+    struct polynomial c;
+
+    printf("a(x) = ");
+    print(a);
+
+    printf("b(x) = ");
+    print(b);
+
+    c = multiply(a, b);
+
+    printf("---------------------------------\n");
+    printf("c(x) = ");
+    print(c);
 
     return 0;
 }
