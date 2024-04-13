32afbd62492ad724011f29d0a0404eb818c02528
한범 김 <mine5139@naver.com>
2024-04-14 01:59:36 +0900

    123

--- Project2.c/FileName.c
+++ Project2.c/FileName.c
@@ -1,24 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void) {
-    int i = 0;
-    int j = 0;
-    int sum = 0;
+#define MAX_STACK_SIZE 5
 
-    for (i = 2; i < 100; i++)
-    {
-        for (j = 2; j < i; j++)
-        {
-            if (i % j == 0)
-                break;
-        }
+typedef int element;
+element stack[MAX_STACK_SIZE];
+int top = -1;
 
+int is_full() {
+    if (top == MAX_STACK_SIZE - 1) return 1;
+    else return 0;
+}
 
-        if (i == j)
-            sum = +i;
-        printf("%d ", i);
+int is_empty() {
+    if (top == -1)  return 1;
+    else return 0;
+}
+
+void push(element item) {
+    if (is_full()) {
+        fprintf(stderr, "스택 포화 에러\n");
+        return;
     }
+    else {
+        top = top + 1;
+        stack[top] = item;
+        printf("push %d\n", item);
+    }
+}
 
+element pop() {
+    element r;
+    if (is_empty()) {
+        fprintf(stderr, " 스택 공백 에러\n");
+        return -1;
+    }
+    else {
+        r = stack[top];
+        top = top - 1;
+        printf("pop %d\n", r);
+        return r;
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    element rend_num;
+
+    for (int i = 0; i < 30; i++) {
+        rend_num = (rand() % 100) + 1;
+        if (rend_num % 2 == 0) {
+            push(rend_num);
+        }
+        else {
+            pop();
+        }
+    }
 
     return 0;
 }
